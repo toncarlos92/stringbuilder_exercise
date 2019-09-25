fc29d4a027c0297a6ea16f80e9e878cdec0163d5
Antonio Carlos <antoniolp_92@hotmail.com>
2019-09-24 22:33:44 -0300

    Exercicio utilizando StringBuilder

---
+++ StringBuilder/Entitites/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace StringBuilder1.Entitites
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
